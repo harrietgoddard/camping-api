17100a360afcc60400380b51828e228c4a75b6de
Harriet Goddard <goddardhe@gmail.com>
2020-06-15 12:36:05 +0100

    Region and Campsite dummy data entered

--- database/migrations/2020_06_15_110018_create_campsites_table.php
+++ database/migrations/2020_06_15_110018_create_campsites_table.php
@@ -17,6 +17,7 @@ class CreateCampsitesTable extends Migration
             $table->id();
             $table->foreignId('region_id')->unsigned();
             $table->foreign('region_id')->references('id')->on('regions')->onDelete('cascade');
+            $table->string('campsite_name', 50)->nullable();
             $table->float('price', 8, 2)->nullable();
             $table->text('description')->nullable();
             $table->string('address', 50)->nullable();
@@ -40,3 +41,18 @@ class CreateCampsitesTable extends Migration
         Schema::dropIfExists('campsites');
     }
 }
+/*
+$campsite = new Campsite();
+$campsite->region_id = 1;
+$campsite->campsite_name = "Henry's Campsite";
+$campsite->price = 25;
+$campsite->description = "We’re situated in a rural area, with a spectacular sea view and sunsets, at the top of caerthillian valley within a couple of minutes walk from the village centre and ten minutes walk to the coastal paths. there are many inland paths to bring you back into the village centre from all directions along the coastal paths and beaches.";
+$campsite->address = "The Lizard";
+$campsite->town_city = "Helston";
+$campsite->postcode = "TR12 7NX";
+$campsite->amenities = "Showers, toilets, cafe";
+$campsite->rating = 3;
+$campsite->contact_no = "01326290596"
+$campsite->contact_email = "info@henryscampsite.co.uk"
+$campsite->save();
+*/
