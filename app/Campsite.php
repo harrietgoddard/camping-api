9d5c86fc8c24aa2420b26f92806e2f914399adbd
Harriet Goddard <goddardhe@gmail.com>
2020-06-15 12:42:23 +0100

    One to many relationship established

--- app/Campsite.php
+++ app/Campsite.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Campsite extends Model
 {
-    //
+    public function region()
+    {
+        return $this->belongsTo(Region::class);
+    }
 }
--- app/Region.php
+++ app/Region.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Region extends Model
 {
-    //
+    public function campsites()
+    {
+        return $this->hasMany(Campsite::class);
+    }
 }
